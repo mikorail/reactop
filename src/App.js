035749d931ee1a4cec247a5c48491633d6b5d4ed
unxplainable <43263583+unxplainable@users.noreply.github.com>
2019-05-07 09:25:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import PrimarySearchAppBar from "./PrimarySearchAppBar";
-
-import Airplane from '@material-ui/icons/AirplanemodeActive';
+import HotelCard from "./HotelCard";
+import SampleMap from './SampleMap';
 
 class App extends Component {
+
     state = {
         open: false,
+        hotels: [
+            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskripsi: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'},
+            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskripsi: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'},
+            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskripsi: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'},
+            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskripsi: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'}
+        ]
     };
 
     handleClickOpen = () => {
@@ -19,9 +26,25 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <PrimarySearchAppBar/>
-                <Airplane className="miring"/>
+            <div className="row">
+                <div className="col m12">
+                    <PrimarySearchAppBar/>
+                    <br/>
+                    <SampleMap/>
+                    <br/>
+                    <div className="row">
+                        <div className="col-md-5">
+                        {this.state.hotels.map((data, key) => {
+                                return (
+                                    <div>
+                                        <HotelCard key={key} title={data.nama} tanggal={data.tanggal} deskripsi={data.deskripsi}/>
+                                        <br/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
